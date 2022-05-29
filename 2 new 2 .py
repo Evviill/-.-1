49c1201032033c667580e24474dc6684c9e0d5e6
Evviill <105318232+Evviill@users.noreply.github.com>
2022-05-29 13:52:33 +0400

    Update and rename 2 new .py to 2 new 2 .py

--- 2 new 2 .py
+++ 2 new 2 .py
@@ -1,7 +1,7 @@
 import time
 K = int(input())
 start_time = time.time()  # ставим таймер на начальное значение ставим его до input что бы не было задержек
-file = list(open('text.txt', 'r').read())  # чтение файла
+file = list(open('text_2.txt', 'r').read())  # чтение файла
 file_out = []
 
 # различные переменные счётчики
@@ -10,13 +10,14 @@ para = 1
 para_s = 1
 all_s = 0
 K_s_p = 0
+K_break = 0
 for i in range(len(file)): # проходим по всему списку
 
 
-    if file[i].isdigit():  # проверка на цыфру так как работаем только с цифрами
+    if file[i].isdigit() :  # проверка на цыфру так как работаем только с цифрами
         all_s += 1
 
-        if int(file[i]) == 0:  # Проверка на K нулей
+        if int(file[i]) == 0 and K_break == 0:  # Проверка на K нулей
             try:
                 for j in range(K): # считаем нули что бы они совпадали с количеством K
                     if int(file[i+j]) == 0:
@@ -28,6 +29,10 @@ for i in range(len(file)): # проходим по всему списку
                     para_s = 2 * K - 1
                     para = K + 3
                     K_s_p = K
+
+                    K_break = 1
+
+
                 else:  # сбрасываем счётчик
                     K_s = 0
 
@@ -40,8 +45,14 @@ for i in range(len(file)): # проходим по всему списку
                     file_out.append('0')
                 file_out.append(file[i-1])
         else:  # если это не та пара то просто выводим её не обрабатывая
-            if para != 1 and K_s_p <= 0:
-                file_out.append(file[i])
+            if K_break == 1:
+                K_break = 2
+                if K == 1:
+                    file_out.append(file[i])
+            else:
+                if para != 1 and K_s_p <= 0 :
+
+                    file_out.append(file[i])
             para -= 1
             K_s_p -= 1
 
